01f4373f07255e8367e3ef56abbdf1533dce0735
Igoruds <igorumene@gmail.com>
2021-08-25 21:57:21 -0300

    Adicionando o shell script do caulefetch

---
+++ myfetch.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+display_info(){
+
+	d_title=$USER@$HOSTNAME
+	d=$(uname -a | awk '{print $2}')
+	d_os=$(uname -o)
+	d_kname=$(cat /proc/sys/kernel/ostype)
+	d_kversion=$(cat /proc/sys/kernel/osrelease)
+	d_uptime=$(uptime | awk '{print $3}')" min"
+	d_shell=$(basename $SHELL)
+	d_res=$(xrandr | sed -n '1p' | sed 's/.*current.//g;s/,.*//g;s/ //g')
+	d_desk=$XDG_SESSION_DESKTOP
+	d_cpu=$(cat /proc/cpuinfo | grep -o 'model name.*' | sed -n 1p | sed 's/.*:.//g;s/(.*)//g')
+	d_mem=$(echo $(cat /proc/meminfo | sed -n 1p | tr -d [A-Za-z:' ']) / 1000000 | bc)" GB"
+	
+}
+
+set_info(){
+display_info
+cat <<EOF
+
+$d_title
+Distro: ${d^}
+OS: $d_os
+Kernel Name: $d_kname
+Kernel Version: $d_kversion
+Uptime: $d_uptime
+Shell: ${d_shell^}
+Resolution: $d_res
+Desk: $d_desk
+CPU: $d_cpu
+RAM: $d_mem
+
+
+
+EOF
+}
+
+logo="
+ ⣿⣿⣿⣿⣿⡟⣛⡻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+ ⣿⣿⣿⣿⣿⣇⠘⢿⣦⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+ ⣿⣿⣿⠿⠿⣿⣷⠰⣽⣶⣮⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣥⣴⣶⣶⣦⣍⢿⣿
+ ⣿⣿⣏⠉⢷⣌⢿⣷⡌⢿⡿⣗⡹⣿⣿⣿⢛⣭⣩⣽⢦⡚⣭⡹⣿⣿⣿⣿⡆⢿
+ ⣿⣿⣿⣦⠨⣫⣮⣝⢿⣌⢿⣿⣿⡝⣿⣿⢸⡿⡝⢇⠿⡇⣿⣷⣭⣛⠻⠇⠇⣾
+ ⣿⣿⣿⣿⣷⣝⢿⣿⣃⣻⢧⠹⣿⣿⡘⡇⡇⠂⣠⣶⣤⣦⡁⠻⣿⣿⡇⠄⢠⣿
+ ⣿⣿⣿⣿⣿⣿⣧⡡⣿⣿⣷⡀⠘⠿⣿⠖⣠⣾⣿⣿⣿⣿⣿⢰⣤⣴⣧⢠⣿⣿
+ ⡿⣛⣛⠿⣿⣿⣿⣿⣮⡻⣿⡟⠄⠈⢁⣼⣿⣿⣿⣿⣿⣿⠏⠄⡻⣿⣿⡆⣿⣿
+ ⣇⣋⠙⢿⣶⣮⣝⡛⠿⢿⡀⠇⠄⣰⣿⣿⣿⣿⣿⣿⡿⠁⢠⣴⣾⣾⣿⡇⣼⣿
+ ⣿⣿⣷⣬⣋⣙⠻⣵⣿⣿⡶⢀⣾⣿⣿⣿⣿⣿⣿⠏⠄⠠⣿⣿⣿⣿⡏⠃⣿⣿
+ ⣿⣿⣿⣿⣿⣿⣿⣮⣙⠙⣰⣿⣿⣿⣿⣿⣿⣿⠋⠂⠄⣼⣿⣿⣿⣿⠁⢰⣿⣿
+ ⣿⣿⣿⣿⣿⣿⣿⣿⠏⣴⣿⣿⣿⣿⣿⣿⡿⠃⠄⢀⢲⣿⣿⣿⣿⡇⠄⣾⣿⣿
+ ⣿⣿⣿⣿⣿⣿⣿⢋⣾⣿⣿⣿⣿⣿⣿⡿⠁⠁⢠⣿⣮⣻⣿⣿⣿⣵⡜⢿⣿⣿
+ ⣿⣿⣿⣿⣿⣿⢃⣾⣿⣿⣿⣿⣿⣿⡟⠐⠄⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡹⢿
+ ⣿⣿⣿⣿⣿⢃⣾⣿⣿⣿⣿⣿⣿⡟⠐⣀⠐⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮
+
+"
+#printf "%s" "$logo"
+#set_info
+paste <(printf "%s" "$logo") <(set_info)
+
